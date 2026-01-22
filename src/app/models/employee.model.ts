d0d596b36da2a9c12c93f71b1922aa72b97be324
Mandar Waigaonkar <mandar.sachin@btech.christuniversity.in>
2026-01-22 15:48:18 +0530

    Add Employee domain model

---
+++ src/app/models/employee.model.ts
@@ -0,0 +1,10 @@
+export interface Employee {
+  id: number;
+  name: string;
+  email: string;
+  department: string;
+  role: string;
+  salary: number;
+  isActive: boolean;
+  dateOfJoining: Date;
+}
